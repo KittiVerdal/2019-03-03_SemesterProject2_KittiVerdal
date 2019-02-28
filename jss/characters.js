f4363796f9fba9ff603910d20c5b72b99292685a
Kitti Verdal <kitti@jbl.no>
2019-02-28 21:15:48 +0100

    Toggeling code reduzed

--- jss/characters.js
+++ jss/characters.js
@@ -27,72 +27,27 @@ function removeValueFromLocalStorage(key){
     localStorage.removeItem(key);
 };
 
+function toggle_information(characterInfo) {
+  document.getElementById('tormundToggle').style.display = 'none';
+  document.getElementById('yrgitteToggle').style.display = 'none';
+  document.getElementById('johnToggle').style.display = 'none';
+  document.getElementById('sansaToggle').style.display = 'none';
+  document.getElementById('aryaToggle').style.display = 'none';
+  document.getElementById('tyrionToggle').style.display = 'none';
+  document.getElementById('cerceiToggle').style.display = 'none';
+  document.getElementById('jamieToggle').style.display = 'none';
+  document.getElementById('daeneyrsToggle').style.display = 'none';
+  document.getElementById('khalToggle').style.display = 'none';
+  var selectedCharacter = document.getElementById(characterInfo);
 
-// Toggeling API information
-function toggle_information(tormundToggle, yrgitteToggle, johnToggle, sansaToggle, aryaToggle, tyrionToggle, cerceiToggle, jamieToggle, daeneyrsToggle, khalToggle) {
+  if (selectedCharacter.style.display === "none"){
+      selectedCharacter.style.display = "block";
+      }
+  else{
+      selectedCharacter.style.display = "none";
+           }
+       }
 
-  var tormund = document.getElementById(tormundToggle);
-  var yrgitte = document.getElementById(yrgitteToggle);
-  var john = document.getElementById(johnToggle);
-  var sansa = document.getElementById(sansaToggle);
-  var arya = document.getElementById(aryaToggle);
-  var tyrion = document.getElementById(tyrionToggle);
-  var cercei = document.getElementById(cerceiToggle);
-  var jamie = document.getElementById(jamieToggle);
-  var daenerys = document.getElementById(daeneyrsToggle);
-  var khal = document.getElementById(khalToggle);
-
-
-  if (tormund.style.display === "none")
-    tormund.style.display = "block";
-  else
-    tormund.style.display = "none";
-
-  if (yrgitte.style.display === "none")
-    yrgitte.style.display = "block";
-  else
-    yrgitte.style.display = "none";
-
-  if (john.style.display === "none")
-    john.style.display = "block";
-  else
-    john.style.display = "none";
-
-  if (sansa.style.display === "none")
-    sansa.style.display = "block";
-  else
-    sansa.style.display = "none";
-
-  if (arya.style.display === "none")
-    arya.style.display = "block";
-  else
-    arya.style.display = "none";
-
-  if (tyrion.style.display === "none")
-    tyrion.style.display = "block";
-  else
-    tyrion.style.display = "none";
-
-  if (cercei.style.display === "none")
-    cercei.style.display = "block";
-  else
-    cercei.style.display = "none";
-
-  if (jamie.style.display === "none")
-    jamie.style.display = "block";
-  else
-    jamie.style.display = "none";
-
-  if (daenerys.style.display === "none")
-    daenerys.style.display = "block";
-  else
-    daenerys.style.display = "none";
-
-  if (khal.style.display === "none")
-    khal.style.display = "block";
-  else
-    khal.style.display = "none";
-}
 
 //API fetch and creating the cards
 
