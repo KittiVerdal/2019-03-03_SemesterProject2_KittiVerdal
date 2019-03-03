d69a06e5263b10a2eb603e2429d93b7da6f2b3d8
Kitti Verdal <kitti@jbl.no>
2019-03-03 09:44:12 +0100

    Characters bugs and js
    Responsive bug fixed and jss reduzed

--- jss/characters.js
+++ jss/characters.js
@@ -1,18 +1,15 @@
 //Variables hide characters
-var selecttormund = ['yrgitte', 'john', 'sansa', 'arya', 'tyrion', 'cercei', 'jamie', 'daeneyrs', 'khal']
-var selectyrgitte = ['tormund', 'john', 'sansa', 'arya', 'tyrion', 'cercei', 'jamie', 'daeneyrs', 'khal']
-var selectjohn = ['tormund', 'yrgitte', 'sansa', 'arya', 'tyrion', 'cercei', 'jamie', 'daeneyrs', 'khal']
-var selectsansa = ['tormund', 'yrgitte', 'john', 'arya', 'tyrion', 'cercei', 'jamie', 'daeneyrs', 'khal']
-var selectarya = ['tormund', 'yrgitte', 'john', 'sansa', 'tyrion', 'cercei', 'jamie', 'daeneyrs', 'khal']
-var selecttyrion = ['tormund', 'yrgitte', 'john', 'sansa', 'arya', 'cercei', 'jamie', 'daeneyrs', 'khal']
-var selectcercei = ['tormund', 'yrgitte', 'john', 'sansa', 'arya', 'tyrion', 'jamie', 'daeneyrs', 'khal']
-var selectjamie = ['tormund', 'yrgitte', 'john', 'sansa', 'arya', 'tyrion', 'cercei', 'daeneyrs', 'khal']
-var selectdaeneyrs = ['tormund', 'yrgitte', 'john', 'sansa', 'arya', 'tyrion', 'cercei', 'jamie', 'khal']
-var selectkhal = ['tormund', 'yrgitte', 'john', 'sansa', 'arya', 'tyrion', 'cercei', 'jamie', 'daeneyrs']
 
-//Charactercount
-var countCharacters = 0;
-
+var selecttormund = ['yrgittePlayer', 'johnPlayer', 'sansaPlayer', 'aryaPlayer', 'tyrionPlayer', 'cerceiPlayer', 'jamiePlayer', 'daeneyrsPlayer', 'khalPlayer'];
+var selectyrgitte = ['tormundPlayer', 'johnPlayer', 'sansaPlayer', 'aryaPlayer', 'tyrionPlayer', 'cerceiPlayer', 'jamiePlayer', 'daeneyrsPlayer', 'khalPlayer'];
+var selectjohn = ['tormundPlayer', 'yrgittePlayer', 'sansaPlayer', 'aryaPlayer', 'tyrionPlayer', 'cerceiPlayer', 'jamiePlayer', 'daeneyrsPlayer', 'khalPlayer'];
+var selectsansa = ['tormundPlayer', 'yrgittePlayer', 'johnPlayer', 'aryaPlayer', 'tyrionPlayer', 'cerceiPlayer', 'jamiePlayer', 'daeneyrsPlayer', 'khalPlayer'];
+var selectarya = ['tormundPlayer', 'yrgittePlayer', 'johnPlayer', 'sansaPlayer', 'tyrionPlayer', 'cerceiPlayer', 'jamiePlayer', 'daeneyrsPlayer', 'khalPlayer'];
+var selecttyrion = ['tormundPlayer', 'yrgittePlayer', 'johnPlayer', 'sansaPlayer', 'aryaPlayer', 'cerceiPlayer', 'jamiePlayer', 'daeneyrsPlayer', 'khalPlayer'];
+var selectcercei = ['tormundPlayer', 'yrgittePlayer', 'johnPlayer', 'sansaPlayer', 'aryaPlayer', 'tyrionPlayer', 'jamiePlayer', 'daeneyrsPlayer', 'khalPlayer'];
+var selectjamie = ['tormundPlayer', 'yrgittePlayer', 'johnPlayer', 'sansaPlayer', 'aryaPlayer', 'tyrionPlayer', 'cerceiPlayer', 'daeneyrsPlayer', 'khalPlayer'];
+var selectdaeneyrs = ['tormundPlayer', 'yrgittePlayer', 'johnPlayer', 'sansaPlayer', 'aryaPlayer', 'tyrionPlayer', 'cerceiPlayer', 'jamiePlayer', 'khalPlayer'];
+var selectkhal = ['tormundPlayer', 'yrgittePlayer', 'johnPlayer', 'sansaPlayer', 'aryaPlayer', 'tyrionPlayer', 'cerceiPlayer', 'jamiePlayer', 'daeneyrsPlayer'];
 
 // Local storage functions
 function resetLocalStorage() {
@@ -28,7 +25,6 @@ function removeValueFromLocalStorage(key) {
 };
 
 //Toggle information
-
 function toggle_information(characterInfo) {
   document.getElementById('tormundToggle').style.display = 'none';
   document.getElementById('yrgitteToggle').style.display = 'none';
@@ -44,8 +40,7 @@ function toggle_information(characterInfo) {
 
   if (selectedCharacter.style.display === "none") {
     selectedCharacter.style.display = "block";
-  }
-   else {
+  } else {
     selectedCharacter.style.display = "none";
   }
 }
@@ -76,7 +71,9 @@ function getCharacterName(Url, id) {
 
 function createPlayerInfo(result, id) {
   var playerInfo = document.getElementById(id);
-  var displayPlayer = '<i class="[ fa fa-times ] [ close ]" onclick="toggle_information()"></i><h2 class="[ characterName--small ]">Title: ' + result.titles[0] + '</h2>' +
+  var displayPlayer =
+    '<i class="[ fa fa-times ] [ close ]" onclick="toggle_information()"></i><h2 class="[ characterName--small ]">Title: ' +
+    result.titles[0] + '</h2>' +
     '<p class="[ characterText ]"><span class="[ characterText--color ]">Culture: </span>' + result.culture + '</p>' +
     '<p class="[ characterText ]"><span class="[ characterText--color ]">Gender: </span> ' + result.gender + '</p>' +
     '<p class="[ characterText ]"><span class="[ characterText--color ]">Aliases: </span>' + result.aliases + '</p></div>';
@@ -91,11 +88,6 @@ function createHeader(result, id) {
 function changeHeader() {
   var reduzeTitle = document.querySelector('.headers');
   reduzeTitle.classList.add("headers__character");
-
-}
-
-function closeToggle() {
-
 }
 
 getCharacterData('https://www.anapioficeandfire.com/api/characters/2024', "tormundToggle");
@@ -122,11 +114,9 @@ function removeTitle() {
 
   var symbolSize = document.querySelectorAll('.characters__symbol');
   for (var i = 0; i < symbolSize.length; i++) {
-    symbolSize[i].style.width = "35%";
-    symbolSize[i].style.float = "none";
-    symbolSize[i].style.display = "block";
-    symbolSize[i].style.margin = "0 auto";
-    symbolSize[i].style.paddingTop = "160px";
+    symbolSize[i].style.width = "30%";
+    symbolSize[i].style.position = "absolute";
+    symbolSize[i].style.right = "20px";
   }
 }
 
@@ -136,88 +126,103 @@ function showButtons() {
   buttons.style.display = "block";
 }
 
+
 //Select player
-/*function displayPlayer(name, character) {
+function displayPlayer(name) {
   document.getElementById(name).style.height = "45vh";
   document.getElementById(name).classList.add("animated");
   document.getElementById(name).classList.add("fadeInUp");
   document.getElementById(name).classList.add("slow");
-  for (var i = 0; i < select + name.length; i++) {
-    document.getElementById(select + name[i]).style.display = "none";
-  }
+
+  /*var deleteCharacter = 'select'+ name;
+  console.log(deleteCharacter);
+  for (var i = 0; i < deleteCharacter.length; i++) {
+    document.getElementById(deleteCharacter[i]).style.display = "none";
+  }*/
+
+  //for (var i = 0; i < ('select' + name).length; i++) {
+  //console.log(('select' + name).lenght);
+  //document.getElementById(('select' + name)[i]).style.display = "none";
+  //}
+
   removeTitle();
   showButtons();
   changeHeader();
   switch (name) {
-    //var t = document.querySelector('.characters__'+ name +');
-    //t.style.height = "45vh";
-    //t.classList.add("animated");
-    //t.classList.add("fadeInUp");
-    //t.classList.add("slow");
     case 'tormund':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/2024', "header");
-      addValuesToLocalStorage("playerTormund", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerTormund", name);
+      for (var i = 0; i < selecttormund.length; i++) {
+        document.getElementById(selecttormund[i]).style.display = "none";
+      }
       break;
     case 'yrgitte':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/2126', "header");
-      addValuesToLocalStorage("playerYrgitte", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerYrgitte", name);
+      for (var i = 0; i < selectyrgitte.length; i++) {
+        document.getElementById(selectyrgitte[i]).style.display = "none";
+      }
       break;
     case 'john':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/583', "header");
-      addValuesToLocalStorage("playerJohn", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerJohn", name);
+      for (var i = 0; i < selectjohn.length; i++) {
+        document.getElementById(selectjohn[i]).style.display = "none";
+      }
       break;
     case 'sansa':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/957', "header");
-      addValuesToLocalStorage("playerSansa", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerSansa", name);
+      for (var i = 0; i < selectsansa.length; i++) {
+        document.getElementById(selectsansa[i]).style.display = "none";
+      }
       break;
     case 'arya':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/148', "header");
-      addValuesToLocalStorage("playerArya", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerArya", name);
+      for (var i = 0; i < selectarya.length; i++) {
+        document.getElementById(selectarya[i]).style.display = "none";
+      }
       break;
     case 'tyrion':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/1052', "header");
-      addValuesToLocalStorage("playerTyrion", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerTyrion", name);
+      for (var i = 0; i < selecttyrion.length; i++) {
+        document.getElementById(selecttyrion[i]).style.display = "none";
+      }
       break;
     case 'cercei':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/238', "header");
-      addValuesToLocalStorage("playerCercei", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerCercei", name);
+      for (var i = 0; i < selectcercei.length; i++) {
+        document.getElementById(selectcercei[i]).style.display = "none";
+      }
       break;
     case 'jamie':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/529', "header");
-      addValuesToLocalStorage("playerJamie", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerJamie", name);
+      for (var i = 0; i < selectjamie.length; i++) {
+        document.getElementById(selectjamie[i]).style.display = "none";
+      }
       break;
     case 'daeneyrs':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/271', "header");
-      addValuesToLocalStorage("playerDaeneyrs", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerDaeneyrs", name);
+      for (var i = 0; i < selectdaeneyrs.length; i++) {
+        document.getElementById(selectdaeneyrs[i]).style.display = "none";
+      }
       break;
     case 'khal':
       getCharacterName('https://www.anapioficeandfire.com/api/characters/1278', "header");
-      addValuesToLocalStorage("playerKhal", character);
-      countCharacters = countCharacters + 1;
-      console.log(countCharacters);
+      addValuesToLocalStorage("playerKhal", name);
+      for (var i = 0; i < selectkhal.length; i++) {
+        document.getElementById(selectkhal[i]).style.display = "none";
+      }
       break;
     default:
-      console.log('There seems to be a problem, please try again later');
+      console.log('There seem to be a problem, please try again later');
   }
-}*/
+}
 
 /*function playerTormund(tormund, character) {
   var t = document.querySelector('.characters__tormund');
