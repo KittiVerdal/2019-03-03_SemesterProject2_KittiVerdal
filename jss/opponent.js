1820bb62a82f5c95ced611258a4a9d33fb355fcb
Kitti Verdal <kitti@jbl.no>
2019-03-03 09:44:36 +0100

    Opponent page responsive
    Css finished

--- jss/opponent.js
+++ jss/opponent.js
@@ -1,15 +1,23 @@
-
 var countCharacters = 1;
+
+
+// Local storage functions
+function resetLocalStorage() {
+  localStorage.clear();
+};
+
+function addValuesToLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+};
+
+function removeValueFromLocalStorage(key) {
+  localStorage.removeItem(key);
+};
 
 //choose opponent
 function selectOpponent(){
   var oppnentSelected = document.getElementById('opponent');
   oppnentSelected.classList.add("btn--primary");
   oppnentSelected.innerHTML = '<i class="[ fa fa-check ] [ validation ]"></i>';
-
-}
-
-function selectPlayerTwo(character){
-
 
 }
