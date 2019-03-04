dde64669fdc73318dcafdb8aa2f29729bb3612fa
Kitti Verdal <kitti@jbl.no>
2019-03-04 13:48:19 +0100

    For feedback

--- jss/characters.js
+++ jss/characters.js
@@ -44,7 +44,6 @@ function toggle_information(characterInfo) {
     selectedCharacter.style.display = "none";
   }
 }
-
 //API fetch and creating the cards
 
 function getCharacterData(Url, id) {
@@ -126,24 +125,12 @@ function showButtons() {
   buttons.style.display = "block";
 }
 
-
 //Select player
 function displayPlayer(name) {
   document.getElementById(name).style.height = "45vh";
   document.getElementById(name).classList.add("animated");
   document.getElementById(name).classList.add("fadeInUp");
   document.getElementById(name).classList.add("slow");
-
-  /*var deleteCharacter = 'select'+ name;
-  console.log(deleteCharacter);
-  for (var i = 0; i < deleteCharacter.length; i++) {
-    document.getElementById(deleteCharacter[i]).style.display = "none";
-  }*/
-
-  //for (var i = 0; i < ('select' + name).length; i++) {
-  //console.log(('select' + name).lenght);
-  //document.getElementById(('select' + name)[i]).style.display = "none";
-  //}
 
   removeTitle();
   showButtons();
@@ -223,191 +210,3 @@ function displayPlayer(name) {
       console.log('There seem to be a problem, please try again later');
   }
 }
-
-/*function playerTormund(tormund, character) {
-  var t = document.querySelector('.characters__tormund');
-  t.style.height = "45vh";
-  t.classList.add("animated");
-  t.classList.add("fadeInUp");
-  t.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/2024', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerTormund", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selecttormund.length; i++) {
-    document.getElementById(selecttormund[i]).style.display = "none";
-  }
-}*/
-/*function playerYrgitte(yrgitte, character) {
-  var y = document.querySelector('.characters__yrgitte');
-  y.style.height = "45vh";
-  y.classList.add("animated");
-  y.classList.add("fadeInUp");
-  y.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/2126', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerYrgitte", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selectyrgitte.length; i++) {
-    document.getElementById(selectyrgitte[i]).style.display = "none";
-  }
-}*/
-/*function playerJohn(john, character) {
-  var j = document.querySelector('.characters__john');
-  j.style.height = "45vh";
-  j.classList.add("animated");
-  j.classList.add("fadeInUp");
-  j.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/583', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerJohn", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selectjohn.length; i++) {
-    document.getElementById(selectjohn[i]).style.display = "none";
-  }
-}*/
-/*function playerSansa(sansa, character) {
-  var s = document.querySelector('.characters__sansa');
-  s.style.height = "45vh";
-  s.classList.add("animated");
-  s.classList.add("fadeInUp");
-  s.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/957', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerSansa", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selectsansa.length; i++) {
-    document.getElementById(selectsansa[i]).style.display = "none";
-  }
-}*/
-/*function playerArya(arya, character) {
-  var a = document.querySelector('.characters__arya');
-  a.style.height = "45vh";
-  a.classList.add("animated");
-  a.classList.add("fadeInUp");
-  a.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/148', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerArya", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selectarya.length; i++) {
-    document.getElementById(selectarya[i]).style.display = "none";
-  }
-}*/
-/*function playerTyrion(tyrion, character) {
-  var ty = document.querySelector('.characters__tyrion');
-  ty.style.height = "45vh";
-  ty.classList.add("animated");
-  ty.classList.add("fadeInUp");
-  ty.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/1052', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerTyrion", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selecttyrion.length; i++) {
-    document.getElementById(selecttyrion[i]).style.display = "none";
-  }
-}*/
-/*function playerCercei(cercei, character) {
-  var c = document.querySelector('.characters__cercei');
-  c.style.height = "45vh";
-  c.classList.add("animated");
-  c.classList.add("fadeInUp");
-  c.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/238', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerCercei", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selectcercei.length; i++) {
-    document.getElementById(selectcercei[i]).style.display = "none";
-  }
-}*/
-/*function playerJamie(jamie, character) {
-  var ja = document.querySelector('.characters__jamie');
-  ja.style.height = "45vh";
-  ja.classList.add("animated");
-  ja.classList.add("fadeInUp");
-  ja.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/529', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerJamie", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selectjamie.length; i++) {
-    document.getElementById(selectjamie[i]).style.display = "none";
-  }
-}*/
-/*function playerDaeneyrs(daeneyrs, character) {
-  var d = document.querySelector('.characters__daeneyrs');
-  d.style.height = "45vh";
-  d.classList.add("animated");
-  d.classList.add("fadeInUp");
-  d.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/271', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerDaeneyrs", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selectdaeneyrs.length; i++) {
-    document.getElementById(selectdaeneyrs[i]).style.display = "none";
-  }
-}*/
-/*function playerKhal(khal, character) {
-  var k = document.querySelector('.characters__khal');
-  k.style.height = "45vh";
-  k.classList.add("animated");
-  k.classList.add("fadeInUp");
-  k.classList.add("slow");
-  getCharacterName('https://www.anapioficeandfire.com/api/characters/1278', "header");
-  removeTitle();
-  showButtons();
-  changeHeader();
-  addValuesToLocalStorage("playerKhal", character);
-  countCharacters = countCharacters + 1;
-  console.log(countCharacters);
-
-  for (var i = 0; i < selectkhal.length; i++) {
-    document.getElementById(selectkhal[i]).style.display = "none";
-  }
-}*/
-
-/*function deleteButton(removeBtn){
-  var removeBtn = document.querySelectorAll('.btn__moreinfo');
-  var i;
-  for (i = 0; i < removeBtn.length; i++) {
-  removeBtn[i].style.display = "none";
-}*/
--- jss/opponent.js
+++ jss/opponent.js
@@ -1,5 +1,5 @@
+//VARIABLES
 var countCharacters = 1;
-
 
 // Local storage functions
 function resetLocalStorage() {
@@ -14,10 +14,47 @@ function removeValueFromLocalStorage(key) {
   localStorage.removeItem(key);
 };
 
-//choose opponent
-function selectOpponent(){
+//FETCH functions
+function getPlayerTwo(Url, id) {
+  fetch(Url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result);
+      createPlayerTwo(result, id);
+    });
+}
+
+//FUNCTIONS
+function activeButton(){
   var oppnentSelected = document.getElementById('opponent');
   oppnentSelected.classList.add("btn--primary");
   oppnentSelected.innerHTML = '<i class="[ fa fa-check ] [ validation ]"></i>';
-
 }
+
+//Create players
+function createPlayerTwo(result, id){
+  var playerTwoInfo = document.getElementById(id);
+  var displayPlayerTwo =
+  '<div class="row"> <div class="[ player ]"> <div class="[ col-xs-1 ]">'+
+  '<img class="[ player__image ] [ animated fadeInUp ]" src="images/' + result.name + '-avatar.jpg" alt="'+ result.name +'"></div>'+
+  '<div class="[ col-xs-11 ][ player--border ]">'+
+  '<h4 class=" [ animated fadeInUp ]"><span class="[ player__character player__character--wildlings ]">'+ result.name + '</span> </h4>'+
+  '<h3 class="[ player__name ]">Player Two</h3>'+
+  '<a class="[ btn btn--small btn--second ][ animated fadeInUp ]" id="'+ result.name +'" onclick="selectPlayerTwo("'+ result.name +'")">Select</a>'+
+  '</div>'+ '</div>'+ '</div>'+ '</div>'+ '<div class="[ row ]">'+ '<div class="[ col-sm-12 ][ border ]"></div>'+
+  '</div>';
+  playerTwoInfo.innerHTML += displayPlayerTwo;
+};
+
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/2024', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/2126', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/583', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/957', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/148', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/1052', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/238', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/529', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/271', "playerTwo");
+getPlayerTwo('https://www.anapioficeandfire.com/api/characters/1278', "playerTwo");
